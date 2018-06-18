14feeff258d414a16e4d618b0327c9a376c45d42
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-06-18 12:26:10 +0300

    Update ht-analytics.js

--- ht-analytics.js
+++ ht-analytics.js
@@ -1,14 +1,16 @@
 class HTAnalytics extends HTMLElement {
-    connectedCallback() {
-        const key = this.getAttribute('key');
-        let gtag = window.createElement("script");
-        gtag.src = `https://www.googletagmanager.com/gtag/js?id=${key}`;
-        gtag.async = true;
-        document.getElementsByTagName('head')[0].appendChild(gtag);
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', key);
+  connectedCallback() {
+    const key = this.getAttribute("key");
+    let script = document.createElement("script");
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${key}`;
+    script.async = true;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
     }
+    gtag("js", new Date());
+    gtag("config", key);
+  }
 }
-customElements.define('ht-analytics', HTAnalytics);
+customElements.define("ht-analytics", HTAnalytics);
